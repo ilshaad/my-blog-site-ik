630ebee90312c730daa6dce18acb6703f0677a54
RechadSalma <rechadsalma123@gmail.com>
2022-06-21 12:52:08 +0100

    ts --init for tsconfig files but kept all setting, this was done becuase I wanted all settings withing tsconfig.json file but commented out just incase I would like to look at it further later on

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          iK v1 Welcome to <a href="https://nextjs.org">Next.js!</a>
+          iK v2 Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
