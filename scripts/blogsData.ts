e7174653a7c38ce9d027139e15f40fac6ebe58e3
RechadSalma <rechadsalma123@gmail.com>
2022-08-06 12:31:27 +0100

    updated date

--- scripts/blogsData.ts
+++ scripts/blogsData.ts
@@ -12,61 +12,61 @@ type blogsDataType = {
 export const blogsData: blogsDataType = {
   blog5: {
     title: "Deploy multiple Docker containers using Github Actions and Heroku",
-    dateDisplay: "2022 September 2nd Friday",
-    dateTime_attribute: "2022-09-02",
+    dateDisplay: "2022 August 7th Sunday",
+    dateTime_attribute: "2022-08-07",
   },
 
   blog6: {
     title: "Create an ascending and descending sort function",
-    dateDisplay: "2022 August 30th Tuesday",
+    dateDisplay: "2022 August 1st Monday",
     dateTime_attribute: "2022-08-30",
   },
 
   blog3: {
     title: "How to create a controlled form component with React-Typescript",
-    dateDisplay: "2022 August 21st Sunday",
-    dateTime_attribute: "2022-08-21",
+    dateDisplay: "2022 July 28th Thursday",
+    dateTime_attribute: "2022-07-28",
   },
 
   blog1: {
     title: "Create timestamp in SQL format",
-    dateDisplay: "2022 August 11th Thursday",
-    dateTime_attribute: "2022-08-11",
+    dateDisplay: "2022 July 18th Monday",
+    dateTime_attribute: "2022-07-18",
   },
 
   blog4: {
     title: "Create an Axios instance",
-    dateDisplay: "2022 August 2nd Tuesday",
-    dateTime_attribute: "2022-08-02",
+    dateDisplay: "2022 July 14th Thursday",
+    dateTime_attribute: "2022-07-14",
   },
 
   blog9: {
     title: "Create a React HOC scaffold",
-    dateDisplay: "2022 August 1st Monday",
-    dateTime_attribute: "2022-08-01",
+    dateDisplay: "2022 July 7th Thursday",
+    dateTime_attribute: "2022-07-07",
   },
 
   blog8: {
     title: "Most common HTML entities you will use",
-    dateDisplay: "2022 July 31st Sunday",
-    dateTime_attribute: "2022-07-31",
+    dateDisplay: "2022 June 30th Thursday",
+    dateTime_attribute: "2022-06-30",
   },
 
   blog7: {
     title: "Capitalise the first character of a word",
-    dateDisplay: "2022 July 29th Friday",
-    dateTime_attribute: "2022-07-29",
+    dateDisplay: "2022 June 21st Tuesday",
+    dateTime_attribute: "2022-06-21",
   },
 
   blog10: {
     title: "Create a dictionary object with Typescript interface",
-    dateDisplay: "2022 July 26th Tuesday",
-    dateTime_attribute: "2022-07-26",
+    dateDisplay: "2022 June 10th Friday",
+    dateTime_attribute: "2022-06-10",
   },
 
   blog2: {
     title: "Parse SQL timestamp",
-    dateDisplay: "2022 July 20th Wednesday",
-    dateTime_attribute: "2022-07-20",
+    dateDisplay: "2022 May 31st Tuesday",
+    dateTime_attribute: "2022-05-31",
   },
 };
