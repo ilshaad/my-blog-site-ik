e58581036446a1f11b5105abcba490fb32bb6e0b
ilshaad <82680145+ilshaad@users.noreply.github.com>
2025-05-23 19:40:37 +0100

    the test is finally working fine. =now I am fixing each test file separately

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,4 +3,7 @@
 
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
+// import "@testing-library/jest-dom/extend-expect";
+
+// jest.setup.ts
+import "@testing-library/jest-dom";
